6a40b06d4e599d96729275c01b70fa31c19e7af4
Kelel1 <kelel1@yahoo.com>
2017-01-11 05:51:33 -0500

    Assignment 7

---
+++ Multiplication/kelder1-week1-007.Multiplication-897641/src/Multiplication.java
@@ -0,0 +1,12 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int X = 1337;
+        int Y = 42;
+        int Z  = X * Y;
+        String toPrint = X + "*" + Y + " = " + Z;
+        System.out.println(toPrint);// Program your solution here. Remember to use variables a and b!
+    }
+
+}
