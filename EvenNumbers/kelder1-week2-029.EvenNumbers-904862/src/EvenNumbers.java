04a4b26a2cec37538bb41b6f79233b630fd81aa0
Kelel1 <kelel1@yahoo.com>
2017-01-15 16:02:47 -0500

    Assignment 29

---
+++ EvenNumbers/kelder1-week2-029.EvenNumbers-904862/src/EvenNumbers.java
@@ -0,0 +1,10 @@
+
+public class EvenNumbers {
+
+    public static void main(String[] args) {
+       int numb = 0;
+        while (numb <= 98) {
+            System.out.println(numb += 2); // Write your code here
+    }
+}
+}
