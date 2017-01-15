e67185c63917083fa4d64402db2e5f75e5134ccc
Kelel1 <kelel1@yahoo.com>
2017-01-15 16:04:34 -0500

    Assignment 30

---
+++ UpTo/kelder1-week2-030.UpToCertainNumber-904896/src/UpToCertainNumber.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+
+public class UpToCertainNumber {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+        int defl = 1;
+        System.out.print("Up to what number? ");
+        int numb = Integer.parseInt(reader.nextLine());
+        while (defl <= numb) {
+            System.out.println(defl);
+            defl++;
+            
+            
+            // Write your code here
+                
+        
+    }
+}
+}
