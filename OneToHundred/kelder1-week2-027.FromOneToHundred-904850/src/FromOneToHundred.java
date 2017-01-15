894ec8f573baa238ef7569ba06b5fb0e8fee6e4c
Kelel1 <kelel1@yahoo.com>
2017-01-14 23:59:36 -0500

    Assignment 27

---
+++ OneToHundred/kelder1-week2-027.FromOneToHundred-904850/src/FromOneToHundred.java
@@ -0,0 +1,10 @@
+
+public class FromOneToHundred {
+
+    public static void main(String[] args) {
+        int numb = 1;
+        while (numb < 101) {
+            System.out.println(numb++);// Write your program here
+    }
+}
+}
