6d14a6ad2480d0fbf4b86838327f5c8d76624385
Kelel1 <kelel1@yahoo.com>
2017-01-16 03:58:40 -0500

    Assignment 46

---
+++ AverageNumbers/kelder1-week3-046.AverageOfGivenNumbers-921653/src/AverageOfGivenNumbers.java
@@ -0,0 +1,19 @@
+
+public class AverageOfGivenNumbers {
+    
+    // implement here again the method of exercise 43
+    public static int sum(int number1, int number2, int number3, int number4) {
+        int answer = number1+number2+number3+number4;
+        return answer;
+    }
+
+    public static double average(int number1, int number2, int number3, int number4) {
+        double ans = sum(number1,number2,number3,number4);
+        return (ans / 4.0);
+    }
+
+    public static void main(String[] args) {
+        double result = average(4, 3, 6, 1);
+        System.out.println("Average: " + result);
+    }
+}
