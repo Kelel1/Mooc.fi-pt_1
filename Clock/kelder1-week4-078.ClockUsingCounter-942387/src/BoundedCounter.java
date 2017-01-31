f267559c7c6140bb0a24c4dd132d486b0c4a8fa5
Kelel1 <kelel1@yahoo.com>
2017-01-30 23:11:16 -0500

    Assignment 78

---
+++ Clock/kelder1-week4-078.ClockUsingCounter-942387/src/BoundedCounter.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kern Elder
+ */
+public class BoundedCounter {
+    private int value;
+    private int upperLimit;
+    
+    public BoundedCounter(int upperLimit) {       
+        this.value = 0;
+        this.upperLimit = upperLimit;
+        
+    }
+    
+    public void next() {
+       this.value++;
+       if (value > upperLimit) {
+           value = 0;          
+       } 
+           
+    }
+    
+    public String toString() {
+        if (value < 10) {
+            return "0" + value ;
+        } else {
+            return "" + value;
+        }
+    
+    }
+    
+    public int getValue() {         
+        return this.value;
+      }
+    
+    public void setValue(int newValue) {        
+            if (newValue >= 0 && newValue <= upperLimit )
+                this.value = newValue;
+        }
+        
+    
+      
+      
+}
+      
+     
+          
+          
+        
+    
+
+            
+        
+        
+        
+    
+    
+    
+
